0a930d9dacf41abcd337449da506cf5bf355d57a
Arthur Trottmann Ramos <arthurtrottmann17@gmail.com>
2023-10-12 17:25:17 -0300

    Update skiplist.h

--- skiplist.h
+++ skiplist.h
@@ -7,9 +7,9 @@
 
 	SKIPLIST* skip_criar(void);
     void skip_inserir(ITEM* item, SKIPLIST* skiplist);
-    void skip_alterar(ITEM* item, SKIPLIST* skiplist);
+    int skip_alterar(ITEM* item, SKIPLIST* skiplist);
     void skip_remover(char* verbete, SKIPLIST* skiplist);
-    void skip_busca(char* verbete, SKIPLIST* skiplist);
+    ITEM* skip_busca(char* verbete, SKIPLIST* skiplist);
     void skip_imprimir(char ch, SKIPLIST* skiplist);
     int skip_vazia(SKIPLIST* skiplist);
     int skip_cheia(SKIPLIST* skiplist);
