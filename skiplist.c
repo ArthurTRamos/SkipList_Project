996c260c8055fd950b16550d80440a552e5e8397
Arthur Trottmann Ramos <arthurtrottmann17@gmail.com>
2023-10-12 17:25:48 -0300

    Update skiplist.c

--- skiplist.c
+++ skiplist.c
@@ -1,19 +1,96 @@
-#ifndef SKIPLIST_H
-	#define SKIPLIST_H
-	
-	#include "item.h"
+#include "skiplist.h"
 
-	typedef struct skiplist_ SKIPLIST;
+typedef struct no_ NO;
 
-	SKIPLIST* skip_criar(void);
-    void skip_inserir(ITEM* item, SKIPLIST* skiplist);
-    int skip_alterar(ITEM* item, SKIPLIST* skiplist);
-    void skip_remover(char* verbete, SKIPLIST* skiplist);
-    ITEM* skip_busca(char* verbete, SKIPLIST* skiplist);
-    void skip_imprimir(char ch, SKIPLIST* skiplist);
-    int skip_vazia(SKIPLIST* skiplist);
-    int skip_cheia(SKIPLIST* skiplist);
-    void skip_apagar(SKIPLIST** skiplist);
-    int gerar_nivel(int level_max);
+struct no_ {
+    ITEM* item;
+    int nivel;
+    NO* proximo;
+    NO* baixo;
+};
 
-#endif
+struct skiplist_ {
+    int levelAtual;
+    int levelmax;
+    NO* upleft;
+};
+
+SKIPLIST* skip_criar(void) {
+
+}
+
+void skip_inserir(ITEM* item, SKIPLIST* skiplist) {
+
+}
+
+int skip_alterar(ITEM* item, SKIPLIST* skiplist) {
+    if((skiplist != NULL) && (skip_vazia(skiplist) == 0)) {
+        NO* sentinela = skiplist->upleft;
+
+        while(sentinela->nivel != 0) {
+            while(strcmp(item_get_verbete(sentinela->proximo->item), item_get_verbete(item)) < 0)
+                sentinela = sentinela->proximo;
+            sentinela = sentinela->baixo;
+        }
+
+        while(strcmp(item_get_verbete(sentinela->proximo->item), item_get_verbete(item)) < 0)
+            sentinela = sentinela->proximo;
+        
+        if(strcmp(item_get_verbete(sentinela->proximo->item), item_get_verbete(item)) == 0) {
+            item_set_definicao(sentinela->item, item_get_definicao(item));
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void skip_remover(char* verbete, SKIPLIST* skiplist) {
+
+}
+
+ITEM* skip_busca(char* verbete, SKIPLIST* skiplist) {
+    if((skiplist != NULL) && (skip_vazia(skiplist) == 0)) {
+        NO* sentinela = skiplist->upleft;
+
+        while(sentinela->nivel != 0) {
+            while(strcmp(item_get_verbete(sentinela->proximo->item), verbete) < 0)
+                sentinela = sentinela->proximo;
+            sentinela = sentinela->baixo;
+        }
+
+        while(strcmp(item_get_verbete(sentinela->proximo->item), verbete) < 0)
+            sentinela = sentinela->proximo;
+
+        if(strcmp(item_get_verbete(sentinela->item), verbete) == 0)
+            return(sentinela->item);
+    }
+    
+    return NULL;
+}
+
+void skip_imprimir(char ch, SKIPLIST* skiplist) {
+
+}
+
+int skip_vazia(SKIPLIST* skiplist) {
+
+}
+
+int skip_cheia(SKIPLIST* skiplist) {
+    NO* no;
+
+    no = (NO*) malloc(sizeof(NO));
+
+    if(no == NULL)
+        return 1;
+    return 0;
+}
+
+void skip_apagar(SKIPLIST** skiplist) {
+
+}
+
+int gerar_nivel(int level_max) {
+
+}
