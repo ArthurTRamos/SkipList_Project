e0569c418b98a369c238472c2702f6a1dba89ba7
Arthur Trottmann Ramos <arthurtrottmann17@gmail.com>
2023-10-12 17:24:54 -0300

    Update skiplist.c

--- skiplist.c
+++ skiplist.c
@@ -1,27 +1,19 @@
-#include "skiplist.h"
+#ifndef SKIPLIST_H
+	#define SKIPLIST_H
+	
+	#include "item.h"
 
-typedef struct no_ NO;
+	typedef struct skiplist_ SKIPLIST;
 
-struct no_ {
-    ITEM* item;
-    int nivel;
-    NO* proximo;
-    NO* baixo;
-};
+	SKIPLIST* skip_criar(void);
+    void skip_inserir(ITEM* item, SKIPLIST* skiplist);
+    int skip_alterar(ITEM* item, SKIPLIST* skiplist);
+    void skip_remover(char* verbete, SKIPLIST* skiplist);
+    ITEM* skip_busca(char* verbete, SKIPLIST* skiplist);
+    void skip_imprimir(char ch, SKIPLIST* skiplist);
+    int skip_vazia(SKIPLIST* skiplist);
+    int skip_cheia(SKIPLIST* skiplist);
+    void skip_apagar(SKIPLIST** skiplist);
+    int gerar_nivel(int level_max);
 
-struct skiplist_ {
-    int levelAtual;
-    int levelmax;
-    NO* upleft;
-};
-
-SKIPLIST* skip_criar(void);
-void skip_inserir(ITEM* item, SKIPLIST* skiplist);
-void skip_alterar(ITEM* item, SKIPLIST* skiplist);
-void skip_remover(char* verbete, SKIPLIST* skiplist);
-void skip_busca(char* verbete, SKIPLIST* skiplist);
-void skip_imprimir(char ch, SKIPLIST* skiplist);
-int skip_vazia(SKIPLIST* skiplist);
-int skip_cheia(SKIPLIST* skiplist);
-void skip_apagar(SKIPLIST** skiplist);
-int gerar_nivel(int level_max);
+#endif
